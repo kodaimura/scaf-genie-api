c2ad5108fc2f139ca54f9a3a21a2a0ada85f728d
kodaimura <kodaimura123@gmail.com>
2025-05-04 15:51:17 +0900

    add PATCH

--- routes.jl
+++ routes.jl
@@ -7,7 +7,7 @@ using ScafGenie.Responses
 frontend_origin = get(ENV, "FRONTEND_ORIGIN", "http://localhost:3000")
 Genie.config.cors_headers["Access-Control-Allow-Origin"] = frontend_origin
 Genie.config.cors_headers["Access-Control-Allow-Credentials"] = "true"
-Genie.config.cors_headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+Genie.config.cors_headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS, PATCH"
 Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
 
 route("/api/accounts/login", method="POST") do
